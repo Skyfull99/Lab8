f2ded80cf89ab4e2a6516931e1d68d774625d792
David Jimenez <josejimenezastorga@gmail.com>
2022-10-07 10:38:33 -0700

    Cliente

--- src/logicadenegocios/Cliente.java
+++ src/logicadenegocios/Cliente.java
@@ -1,5 +1,51 @@
 package logicadenegocios;
+import java.util.ArrayList;
 
 public class Cliente {
+    private String cedula;
+    private String nombre;
+    private String direccion;
     
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String pNombre){
+        nombre = pNombre;
+    }
+    
+    public String getCedula(){
+        return cedula;
+    }
+    
+    public void setCedula(String pCedula){
+        cedula = pCedula;
+    }
+    
+    public String getDireccion(){
+        return direccion;
+    }
+    
+    public void setDireccion(String pDireccion){
+        direccion = pDireccion;
+    }
+    
+    
+    
+    public Cliente (String pNombre, String pCedula, String pDireccion){
+        
+    }
+    
+    public void registrarNuevoPrestamo (String pNombre, String pCedula, String pDireccion){
+        
+    }
+    
+   // public metodosAccesores (){
+        
+    //}
+    
+  //  public ArrayList<Libro> consultarLibrosConAtrasos (){
+        
+   // }
 }
