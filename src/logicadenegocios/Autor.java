acf6b4d1699953f5c457fc4b74d1d7549c661a95
OBANDO JIMENEZ SEBASTIAN DE JESUS <sebasoj13@estudiantec.cr>
2022-10-07 10:58:00 -0600

    Libro y autor

--- src/logicadenegocios/Autor.java
+++ src/logicadenegocios/Autor.java
@@ -1,12 +1,37 @@
 package logicadenegocios;
 
+
 public class Autor {
-    private int indentificador;
+    private int identificador;
     private String nombre;
     private String nacionalidad;
     
-    public Autor (int pindentificador, String pnombre ){
-        
+    public Autor(int pIdentificador, String pNombre){
+        setIdentificador(pIdentificador);
+        setNombre(pNombre);
     }
     
+    public void setIdentificador(int pIdentificador){
+        identificador = pIdentificador;
+    }
+    
+    public int getIdentificador(){
+        return identificador;
+    }
+    
+    public void setNombre(String pNombre){
+        nombre = pNombre;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNacionalidad(String pNacionalidad){
+        nacionalidad = pNacionalidad;
+    }
+    
+    public String getNacionalidad(){
+        return nacionalidad;
+    }
 }
--- src/logicadenegocios/Libro.java
+++ src/logicadenegocios/Libro.java
@@ -1,4 +1,56 @@
 package logicadenegocios;
+
+
 public class Libro {
+    private String nombre;
+    private String editorial;
+    private int aNoPublicacion;
+    private int identificador;
+    private int cantidadEjemplares = 1;
     
+    public Libro(String pNombre, int pIdentificador, Autor pAutor){
+        setNombre(pNombre);
+        setIdentificador(pIdentificador);
+        
+    }
+    
+    public void setNombre(String pNombre){
+        nombre = pNombre;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setEditorial(String pEditorial){
+        editorial = pEditorial;
+    }
+    
+    public String getEditorial(){
+        return editorial;
+    }
+    
+    public void setAnoPublicacion(int pAnoPublicacion){
+        aNoPublicacion = pAnoPublicacion;
+    }
+    
+    public int getAnoPublicacion(){
+        return aNoPublicacion;
+    }
+    
+    public void setIdentificador(int pIdentificador){
+        identificador = pIdentificador;
+    }
+    
+    public int getIdentificador(){
+        return identificador;
+    }
+    
+    public void setCantidadEjemplares(int pCantEjemplares){
+        cantidadEjemplares = pCantEjemplares;
+    }
+    
+    public int getCantidadEjemplares(){
+        return cantidadEjemplares;
+    }
 }
