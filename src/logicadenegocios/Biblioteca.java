c18962c1268beaca8c7b755047f6fc64738ccb99
David Jimenez <josejimenezastorga@gmail.com>
2022-10-11 18:02:30 -0700

    Biblioteca

--- src/logicadenegocios/Biblioteca.java
+++ src/logicadenegocios/Biblioteca.java
@@ -1,10 +1,13 @@
 package logicadenegocios;
+
 import java.util.ArrayList;
 
 public class Biblioteca {
     private String nombre;
-    private ArrayList<Cliente> cliente;
-     
+    private ArrayList<Cliente> clientes;
+    private ArrayList<Libro> libros;
+    private ArrayList<Autor> autores;
+   
      public String getNombre(){
         return nombre;
     }
@@ -14,23 +17,99 @@ public class Biblioteca {
     }
     
     public Biblioteca (String pNombre){
-        
+        this.nombre = pNombre;
+        clientes = new ArrayList<Cliente>();
+        libros = new ArrayList<Libro>();
+        autores = new ArrayList<Autor>();
     }
     
-    public void registrarLibro(int pIdentificador){
-        
+    public void registrarLibro(int pIdentificador, String pNombre, String pEditorial, int pANoPublicacion) throws BookAlreadyExistException{
+        for(Libro libro : libros){
+            if(libro.getIdentificador() == pIdentificador){
+                throw new BookAlreadyExistException(pIdentificador);
+            }
+        }
+        Libro libro = new Libro(pNombre, pEditorial, pANoPublicacion, pIdentificador);
+        libros.add(libro);
     }
     
-    public void Autor(int pIdentificador){
-        
+    public void registrarAutor(int pIdentificador, String pNombre, String pNacionalidad) throws AuthorAlreadyExistException{
+        for(Autor autor : autores){
+            if(autor.getIdentificador() == pIdentificador){
+                throw new AuthorAlreadyExistException(pIdentificador);
+            }
+        } 
+        Autor autor = new Autor(pIdentificador, pNombre, pNacionalidad);
+        autores.add(autor);
     }
-    
-    public void Cliente(int pIdentificador){
-        
+  
+    public void registrarCliente(int pCedula, String pNombre, String pNacionalidad) throws ClientAlreadyExistsException{
+        for(Cliente cliente : clientes){
+            if(cliente.getCedula() == pCedula){
+                throw new ClientAlreadyExistsException(pCedula);
+            }
+        }
+        Cliente cliente = new Cliente(pCedula, pNombre, pNacionalidad);
+        clientes.add(cliente);
+  }
+
+    public void prestarLibro(int pIdentificador, int pCedula) throws ClientDoesNotExistException, BookDoesNotExistException{
+        Libro testLibro = null;
+        Cliente testCliente = null;
+        for(Cliente cliente : clientes){ 
+            for(Libro libro : libros){
+                if(libro.getIdentificador() == pIdentificador && cliente.getCedula() == pCedula){
+                    testLibro = libro;
+                    testCliente = cliente;
+                    cliente.registrarNuevoPrestamo(libro);
+                }
+            }
+        }
+        if(testLibro == null){
+            throw new BookDoesNotExistException(pIdentificador);
+        }
+        if(testCliente == null){
+            throw new ClientDoesNotExistException(pCedula);
+        }
     }
-    
-    public void Libro(int pIdentificadorCliente, int pIdentificadorLibro){
-        
+  
+    public void buscarLibro(int pIdentificador) throws BookDoesNotExistException{
+        Libro test = null;
+        for(Libro libro : libros){
+            if(libro.getIdentificador() == pIdentificador){
+                test = libro;
+                System.out.println(libro.toString());
+            }
+        }
+        if(test == null){
+            throw new BookDoesNotExistException(pIdentificador);
+        }
+    }
+  
+    public void buscarCliente(int pCedula) throws ClientDoesNotExistException{
+        Cliente test = null;
+        for(Cliente cliente : clientes){
+            if(cliente.getCedula() == pCedula){
+                test = cliente;
+                System.out.println(cliente.toString());
+            }
+        }
+        if(test == null){
+            throw new ClientDoesNotExistException(pCedula);
+        }
+    }
+  
+    public void buscarAutor(int pIdentificador) throws AuthorDoesExistsException{
+        Autor test = null;
+        for(Autor autor : autores){
+            if(autor.getIdentificador() == pIdentificador){
+                test = autor;
+                System.out.println(autor.toString());
+            }
+        }
+        if(test == null){
+            throw new AuthorDoesExistsException(pIdentificador);
+        }
     }
 }
 
